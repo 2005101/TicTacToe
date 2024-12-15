e5d1a3a21d046dc47d5f850cab816aa1749a3974
Amina EL Guenuni <88779300+2005101@users.noreply.github.com>
2024-12-15 22:06:22 +0100

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 public class main {
     public static void main(String[] args) {
-        TicTacToe tictactoe=new TicTacToe(); //object declaration
+        new StartPage(); // start the page then move along the code 
         
         
     }
